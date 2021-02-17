efb59e45169bd18e8273d442acef6ce3cac2dfde
CanlinJiang <44628729+CanlinJiang@users.noreply.github.com>
2021-02-16 23:46:19 -0800

    changes

---
+++ Class4_example2/Class4_example2/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Class4_example2
+//
+//  Created by little tree on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    var textOfField : UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func alertAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Sample Alert", message: "BIBIBIBI", preferredStyle: .alert)
+        let OK = UIAlertAction(title: "OK", style: .default, handler: {(alerAction) in
+            self.label.text = self.textOfField?.text
+            print("OK pressed")
+        })
+        let cancel = UIAlertAction(title: "Canceled", style: .cancel, handler: {(alerAction) in
+            print("Cencel pressed")
+        })
+        
+        alert.addAction(OK)
+        alert.addAction(cancel)
+        
+        alert.addTextField{(textField) in
+            textField.placeholder = "Type Content"
+            self.textOfField = textField
+        }
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
+
