9fbc7dfc86e7e36bad85d4d412b1e3278a856fa1
CanlinJiang <44628729+CanlinJiang@users.noreply.github.com>
2021-02-16 21:36:54 -0800

    sds
    sdsd

---
+++ Class4_example/Class4_example/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  Class4_example
+//
+//  Created by little tree on 2/16/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelCity: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
